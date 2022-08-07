cc5f7e58c48214eac02ae37e3d94024b236f6a94
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-07 21:51:07 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello')
+import requests
+import json
+
+data = {'name': 'Fara',
+        'surname': 'Tutkabaev'}
+
+
+r = requests.post('http://127.0.0.1:5000/webhook', data=json.dumps(data), headers={'Content-Type': 'application/json'})
